a2b6763f592ee7097ad340e52541f8420053f8e5
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2024-08-20 21:50:47 +0530

    Still Building...

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,42 +1,38 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText, StreamData } from 'ai';
-import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
 
-const openAI = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-})
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-export const maxDuration = 30;
+export const runtime = 'edge';
 
 export async function POST(req: Request) {
   try {
-    const { messages } = await req.json();
-  
-    const data = new StreamData();
-    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '|'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started? | | If you could have dinner with any historical figure, who would it be? || What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
-  
-    const result = await streamText({
-      model: openai('gpt-4-turbo-instruct'),
-      maxTokens: 400,
-      messages,
-      onFinish() {
-        data.close();
-      },
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    const response = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      max_tokens: 400,
+      stream: true,
       prompt,
     });
-  
-    return result.toAIStreamResponse({ data });
+
+    const stream = OpenAIStream(response);
+    
+    
+    return new StreamingTextResponse(stream);
   } catch (error) {
     if (error instanceof OpenAI.APIError) {
-        const {name, status, headers, message} = error
-        return NextResponse.json({
-            name, status, headers, message
-        }, {status})
+      // OpenAI API error handling
+      const { name, status, headers, message } = error;
+      return NextResponse.json({ name, status, headers, message }, { status });
     } else {
-        
+      // General error handling
+      console.error('An unexpected error occurred:', error);
+      throw error;
     }
-    console.error("Unexpected Error", error);
-    throw error
   }
 }
--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,22 +1,22 @@
 import { resend } from "@/lib/resend";
-import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../../ama-app/emails/VerificationEmail";
+import { ApiResponse } from '@/types/ApiResponse';
 
-export async function sendVerificationemail(
-    email: string,
-    username: string,
-    verificationCode: string
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
 ): Promise<ApiResponse> {
-    try {
-        await resend.emails.send({
-            from: 'onboarding@resend.dev',
-            to: email,
-            subject: 'Verification Email',
-            react: VerificationEmail({username, otp: verificationCode}),
-          });
-        return {success: true, message: "Verification email sent"};
-    } catch (emailError) {
-        console.log("Error sending verification email: ", emailError);
-        return {success: false, message: "Failed to send verification email"};
-    }
+  try {
+    await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: 'Verification Code for Feed-Back App',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: 'Verification email sent successfully.' };
+  } catch (emailError) {
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+  }
 }
