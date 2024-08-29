fa0845af26ae85040d6133ff6637463364b8d23b
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2024-08-30 00:11:56 +0530

    Update sendVerificationEmail.ts

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'Acme <onboarding@resend.dev>',
       to: email,
       subject: 'Verification Code for Feed-Back App',
       react: VerificationEmail({ username, otp: verifyCode }),
